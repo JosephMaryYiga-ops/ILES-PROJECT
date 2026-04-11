e9d5884eb807691e683f02814a8dd0cc43a2dc7e
JosephMaryYiga-ops <josephmaryyiga@gmail,com>
2026-04-11 12:18:56 +0300

    Configure PostgreSQL and update settings

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ILES_db',
+        'USER': 'postgres',
+        'PASSWORD': 'jose18marie',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -116,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'accounts.User'
