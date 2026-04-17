44e4762e90b353a80e91b4dd814188ac6b942457
Mubiru720 <mubiruelijah@gmail.com>
2026-04-17 15:07:48 +0300

    created API endpoints

--- BACKEND/accounts/views.py
+++ BACKEND/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation, Notification
+from .serializers import UserSerializer, InternshipPlacementSerializer, WeeklyLogSerializer, EvaluationCriteriaSerializer, EvaluationSerializer, NotificationSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class InternshipPlacementViewSet(viewsets.ModelViewSet):
+    queryset = InternshipPlacement.objects.all()
+    serializer_class = InternshipPlacementSerializer
+    
+class WeeklyLogViewSet(viewsets.ModelViewSet):
+    queryset = WeeklyLog.objects.all()
+    serializer_class = WeeklyLogSerializer
+    
+class EvaluationCriteriaViewSet(viewsets.ModelViewSet):
+    queryset = EvaluationCriteria.objects.all()
+    serializer_class = EvaluationCriteriaSerializer
+    
+class EvaluationViewSet(viewsets.ModelViewSet):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer                
+    
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
