7b210bc3a7ec8b43f7edc360db0b95657f3feb6f
shantinahnyerere-web <shantinahnyerere@gmail.com>
2026-04-27 00:22:27 +0300

    Sign up and login form

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,17 +1,12 @@
-
-import'./App.css';
-import LoginSignup from './assets/LoginSignup';
+import "./App.css";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 function App() {
-  
-
   return (
     <div>
       <LoginSignup></LoginSignup>
     </div>
-    
-              
   );
 }
 
-export default App
+export default App;
