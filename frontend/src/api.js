ed586e6adf1d4d8292b2c3910a2d13045fff13b5
bagonza-julius <bagonzajulius19@gmail.com>
2026-05-21 20:49:36 +0300

    missing axios and vite instance

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api',
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Automatically attach JWT token to every request
