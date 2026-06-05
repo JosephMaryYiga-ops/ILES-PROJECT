371cdcb434c00a7995e1de99a9eb8cbf30fe3f68
bagonza-julius <bagonzajulius19@gmail.com>
2026-06-05 09:09:42 +0300

    audit trial

---
+++ BACKEND/accounts/middleware.py
@@ -0,0 +1,15 @@
+from .models import AuditLog
+
+class AuditMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        if request.user.is_authenticated and request.method == 'POST':
+            AuditLog.objects.create(
+                actor=request.user,
+                action=f"{request.method} {request.path}"
+            )
+        return response
