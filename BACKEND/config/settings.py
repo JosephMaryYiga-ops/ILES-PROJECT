edabd970da1b172fd0c7db876bbc98ba385432f2
bagonza-julius <bagonzajulius19@gmail.com>
2026-06-08 09:35:56 +0300

    fixing password verification

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -102,12 +102,7 @@ DATABASES = {
 # ──────────────────────────────────────────
 AUTH_USER_MODEL = 'accounts.User'
 
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
+
 
 # ──────────────────────────────────────────
 # DJANGO REST FRAMEWORK
